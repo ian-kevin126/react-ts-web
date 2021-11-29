98c0365b33d6688645a70408f484f78b5ebdf1e5
ian_kevin126 <ian_kevin126@126.com>
2021-11-29 22:18:43 +0800

    feat：提交文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// 模组化css文件，先要在custom.d.ts中声明，然后将css文件的后缀改为.module.css
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <a
+        className={styles.appLink}
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
